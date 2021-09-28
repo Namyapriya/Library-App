4d674e604d0ed221084e85e5cc8e183aee72df92
Namyapriya Dayananda <35521132+Namyapriya@users.noreply.github.com>
2021-09-29 01:50:17 +0530

    Update SearchScreen.js

--- SearchScreen.js
+++ SearchScreen.js
@@ -7,7 +7,7 @@ import {
   TextInput,
   TouchableOpacity,
 } from 'react-native';
-import db from '../config';
+import db from 'config';
 import { ScrollView } from 'react-native-gesture-handler';
 
 export default class Searchscreen extends React.Component {
